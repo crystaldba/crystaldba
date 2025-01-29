b1941c8dab49be7f68bb452479c8ec23034a14bf
Mohammad Dashti <mdashti@gmail.com>
2025-01-29 12:28:52 -0800

    Fixed the package name usage.

--- agent/crystaldba/shared/secure_session.py
+++ agent/crystaldba/shared/secure_session.py
@@ -23,7 +23,7 @@ from requests_http_signature import HTTPSignatureAuth
 from crystaldba.shared.base64id import generate_b64id
 from crystaldba.shared.constants import HTTP_SIGNATURE_MAX_AGE_SECONDS
 
-__version__ = importlib.metadata.version("crystaldba")
+__version__ = importlib.metadata.version("crystaldba_cli")
 
 
 class ModelDumpProtocol(Protocol):
