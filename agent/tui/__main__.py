7215a7dff8f20130818f0f97102727c95cf7c2ad
Greg Edwards <greg@greglearns.com>
2025-02-19 16:48:38 -0700

    tui: cli arguments: fix

--- agent/tui/__main__.py
+++ agent/tui/__main__.py
@@ -4,8 +4,6 @@ Crystal DBA Text UI (TUI)
 
 import logging
 
-import click
-from click_default_group import DefaultGroup
 from rich.console import Console
 
 from crystaldba.cli import startup
@@ -14,41 +12,14 @@ from tui.app import Tui
 console = Console()
 
 
-@click.group(cls=DefaultGroup, default="default", default_if_no_args=True)
 def cli() -> None:
-    """Your AI teammate with PostgreSQL expertise."""
-
-
-@cli.command()
-@click.option(
-    "-i",
-    "--inline",
-    is_flag=True,
-    help="Run in inline mode, without launching full TUI.",
-    default=False,
-)
-@click.argument("dburi", nargs=-1, type=str, required=False)
-@click.option("--profile", type=str, default="", help="The user profile to use for the chat")
-@click.option("-h", "--host", type=str, default="", help='database server host or socket directory (default: "localhost")')
-@click.option("-p", "--port", type=int, default=5432, help='database server port (default: "5432")')
-@click.option("-U", "-u", "--user", type=str, default="", help='database user name (default: "postgres")')
-@click.option("-d", "--dbname", type=str, default="", help='database name (default: "postgres")')
-def default(
-    inline: bool,
-    dburi: tuple[str, ...],
-    profile: str,
-    host: str,
-    port: int,
-    user: str,
-    dbname: str,
-) -> None:
     chat_turn = startup.startup(
         log_path="log.log",
         logging_level=logging.INFO,
     )
 
     app = Tui(chat_turn=chat_turn)
-    app.run(inline=inline)
+    app.run(inline=False)
 
 
 if __name__ == "__main__":
